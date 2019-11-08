652035a21327b241341b3a60081113232ac96966
Victor <massotte.v@gmail.com>
2019-11-08 16:34:19 +0100

    Commit Boutique

---
+++ BDESite/boutique/admin/admin.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/style.css" />
+
+        <link rel="stylesheet" type="text/css" href="css/styleConnexion.css" />
+        <title>Menu Admin Boutique</title>
+    </head>
+
+    <body>
+  
+    <div id="corps">
+
+        <div id="contenu">
+
+        <div id="wrapper" style="min-height: 50px;"><h1> Menu Admin Boutique</h1></div>
+
+        <div id="barre_boutons_admin">
+            <div class="bouton_admin">  <div style="height:50px;"><img src="Images/shoesList.png" alt="Shoe" height="75"  ></div>  
+            <a href="afficher_produits.php">Lister les articles</a>  </div>
+
+            <div class="bouton_admin"> <div style="height:50px;"><img src="Images/shoesAdd.png" alt="Shoe" height="75" ></div>
+            <a href="addproduit.php">Ajouter un article</a> </div>
+
+            <div class="bouton_admin"> <div style="height:50px;"><img src="Images/shoesDel.png" alt="Shoe" height="75" ></div>
+            <a href="delete.php">Supprimer un article </a></div>
+
+            <div class="bouton_admin"> <div style="height:50px;"><img src="Images/shoesChange.png" alt="Shoe" height="75" ></div>
+            <a href="modifier.php">Modifier un article </a></div>
+
+            <div class="bouton_admin"> <div style="height:50px;"><img src="Images/chaussure_tri.png" alt="Shoe" height="75" ></div>
+            <a href="trierprix.php">Trier par prix </a></div>
+
+            <div class="bouton_admin"> <div style="height:50px;"><img src="Images/cherche.png" alt="Shoe" height="75" ></div>
+            <a href="recherche.php">Chercher un produit </a></div>
+            </div>
+                
+        </div>
+    </div>  
+    
+    </body>
+
+</html>
---
+++ BDESite/boutique/admin/index.php
@@ -0,0 +1,31 @@
+<?php
+
+$email = "victor@viacesi.fr";
+$password ="1234";
+
+if(isset($_POST['submit'])){
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+if($email $$ $password){
+    
+}
+
+}
+
+?>
+
+<h1>Connexion à la page Administration </h1>
+
+    <form action="" method="POST">
+        <h3>Email : </h3>
+        <input type="text" name="email"></br>
+        <h3>PassWord :</h3>
+        <input type="password" name="password"></br>
+        <input type="submit" name="submit">
+        
+    </form>
+
+
+
