43840a89e9eee66da96edc3402f5d736b8a12803
Victor <massotte.v@gmail.com>
2019-11-08 16:52:19 +0100

    Commit Boutique

--- BDESite/boutique/admin/index.php
+++ BDESite/boutique/admin/index.php
@@ -1,26 +1,8 @@
-<?php
-
-$email = "victor@viacesi.fr";
-$password ="1234";
-
-if(isset($_POST['submit'])){
-
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-if($email $$ $password){
-    
-}
-
-}
-
-?>
-
 <h1>Connexion à la page Administration </h1>
 
-    <form action="" method="POST">
+    <form action="scriptconnection" method="POST">
         <h3>Email : </h3>
-        <input type="text" name="email"></br>
+        <input type="text" name="mail"></br>
         <h3>PassWord :</h3>
         <input type="password" name="password"></br>
         <input type="submit" name="submit">
