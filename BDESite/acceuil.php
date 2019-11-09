1eff3fe71d57266cf65b4e8598ebc16bd589a069
Gauthier <gauthier.sannier@viacesi.fr>
2019-11-09 14:49:52 +0100

    commit menu

--- BDESite/acceuil.php
+++ BDESite/acceuil.php
@@ -1,25 +1,24 @@
-      <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="carteConnexion.css">
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>      
-       
-        
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
         <title>Acceuil</title>
+</head>
+<body>
+    <header>
+        <!--en tête-->
+        <!--menu-->
+        <?php include("modules/menu.php") ?>
+    </header>
 
-        <!-- Fonts -->
-        
-
-        <!-- Styles -->
-       
-    </head>
-    <body>
- 
-    </body>
-
+    <!--corps du site-->
+    <footer>
+        <!--pied de page-->
+    </footer>
+</body>
 </html>
