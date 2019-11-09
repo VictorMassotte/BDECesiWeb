29dd029f002145a239ef290bb2c1470443e70a9e
Akorus1 <Phiuris1@gmail.com>
2019-11-09 14:47:12 +0100

    Commit by Kévin 09/11 module de connexion

--- BDESite/Module_Connexion_Inscription/Deconnexion.php
+++ BDESite/Module_Connexion_Inscription/Deconnexion.php
@@ -1,25 +1,19 @@
+<?php session_destroy() ?>
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="fr">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="carteConnexion.css">
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>      
        
         
-        <title>Acceuil</title>
-
-        <!-- Fonts -->
-        
-
-        <!-- Styles -->
-       
+        <title>Déconnexion</title>
+<body>
+        <div class="container">
+        <span> Vous êtes déconnecté <span>
+</body>       
     </head>
-    <body>
- 
-    </body>
-
-</html>
+ </html>   
