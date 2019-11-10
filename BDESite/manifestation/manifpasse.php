330ca9c3ab31932a3bf9d7dd980bc4f7c832fe1b
Gauthier <gauthier.sannier@viacesi.fr>
2019-11-10 18:23:13 +0100

    Ajout des m=pages manifestions, et condition pour voir si l'evenement est passé ou non

---
+++ BDESite/manifestation/manifpasse.php
@@ -0,0 +1,10 @@
+<?php
+$dateactuelle = new DateTime('now');//2019-11-10
+$datetime1 = new DateTime('2019-11-15');//à la place il faut mettre la date de l'evenement récupéré de la bdd dans le format y-m-d
+$interval = $datetime1->diff($dateactuelle);
+if($interval->format('%R%a')<0){
+    // ne rien faire
+}else{
+    echo"positif <br> donc à placer dans evenements passés";
+}
+?>
