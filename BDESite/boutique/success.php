8dc8fd421eec61e212327837674620288d748aef
Victor <massotte.v@gmail.com>
2019-11-11 22:13:49 +0100

    Commit Boutique

--- BDESite/boutique/success.php
+++ BDESite/boutique/success.php
@@ -1,5 +1,13 @@
-<?php
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>  
 
-    echo " <h2>Merci d'avoir commande sur la boutique du CESI BDDE. Un email vous a ete envoye </h2> ";
-
-?>
+<div class="jumbotron text-center">
+  <h1 class="display-3">Merci beaucoup</h1>
+  <p class="lead"><strong>Merci de consulter vos emails. </strong> Pour plus d'informations sur votre commande</p>
+  <hr>
+  <p class="lead">
+    <a class="btn btn-primary btn-sm" href="http://localhost/BDECesiWeb/BDESite/boutique/" role="button">Retour à la boutique</a>
+  </p>
+</div>
