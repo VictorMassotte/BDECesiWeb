cd4c1eeeec05d514d60fe417aad8cc08dfe7d827
quentinbutel <44508495+quentinbutel@users.noreply.github.com>
2019-11-12 18:31:29 +0100

    commit

--- BDESite/js/inscrit.js
+++ BDESite/js/inscrit.js
@@ -1,12 +1,15 @@
 $(function() {
-     
-  var inscrit = $('#like');
+  $('button').click(function(){
+    var x = $(this).attr('id');
+    
+});
+ /* var inscrit = $('#like');
   inscrit.on('load', function(){
     $.ajax({
       url: '../manifestations/like.php',
 
     });
       inscrit.text('Aimé!');
-  });
+  });*/
    
   });
