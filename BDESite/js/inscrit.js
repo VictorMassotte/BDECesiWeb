ca982011a21c3987f47e91d07ed31df04eea6254
quentinbutel <44508495+quentinbutel@users.noreply.github.com>
2019-11-13 09:42:41 +0100

    commit

---
+++ BDESite/js/inscrit.js
@@ -0,0 +1,34 @@
+$(function() {
+ 
+    $('button').click(function(){
+      var x = $(this).attr('id');
+      
+      x = x.replace('inscrit', '');
+      
+      x = x.split('-');
+      id = x[0];
+      nom = x[1];
+      test = ".inscrit";
+      test = test.concat('', id);
+    
+     
+      $.ajax({
+        url: '../manifestation/inscrit.php',
+        type: 'POST',
+        data: { id_manifestation: id, manif: nom, name: "Sannier", firstname: "Gauthier" },
+      })
+      .done(function (data) {
+        
+           
+       //$(test).html(data);
+        successFunction(data); })
+      .fail(function (jqXHR, textStatus, errorThrown) { 
+        alert("fail");
+        serrorFunction(); 
+      });
+     
+      
+  });
+  
+   
+    });
--- BDESite/manifestation/manifavenir.php
+++ BDESite/manifestation/manifavenir.php
@@ -7,6 +7,7 @@
         <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="../js/inscrit.js"></script>
         <link rel="stylesheet" href="css/fonction.css">
         <title>Evenements à venir</title>
 </head>
@@ -19,7 +20,7 @@
 <?php
 $bdd = new PDO('mysql:host=localhost;dbname=projet_web;charset=utf8', 'root', '');
 $response = $bdd->query('SELECT ID,DATEE FROM manifestations ORDER BY DATEE');
-              
+$message =""; 
 while ($ligne = $response->fetch()) {
     echo"<br>";
     $dateactuelle = new DateTime('now');
@@ -37,7 +38,7 @@ while ($ligne = $response->fetch()) {
             $datem=$datebrut[1];
             $datej=$datebrut[2];
             $dateAffichable=$datej."/".$datem."/".$datey;
-            
+            $identifiant=$reponse['ID'];
             $nom=$reponse['NOM'];
             
             $desc=$reponse['DESCRIPTION'];
@@ -45,7 +46,25 @@ while ($ligne = $response->fetch()) {
             $prix=$reponse['PRIX'];
             
             $urlimg=$reponse['IMAGE'];
+            $user = 2;
+
+            $rqtSpe = $bdd->prepare('SELECT * FROM inscrire WHERE ID_USERS=:idU AND ID=:idM');
+            $rqtSpe->bindValue(':idU',$user, PDO::PARAM_STR);
+            $rqtSpe->bindValue(':idM',$identifiant, PDO::PARAM_STR);
+            $rqtSpe->execute();
+            $ligne = $rqtSpe->fetch();
+            if($ligne){  
+                //l'utilisateur à deja liké, on ne fait rien ou on suggère de dislike
+                $message = "Inscrit"; 
+            }
+            else{
+                //on envoie la requête dans la bdd
+                $message = "Je m'inscrit";
+            }  
             
+            
+            $rqtSpe->closeCursor();
+
             echo "
             <div class=\"card text-center text-white bg-dark\">
             <div class=\"card-header\">
@@ -55,7 +74,7 @@ while ($ligne = $response->fetch()) {
             <img src\"$urlimg\" class=\"card-img-top\" alt=\"Image de la manifestation\">
             <h5 class=\"card-title\"> Le $dateAffichable</h5>
             <p class=\"card-text\">$desc</p>
-            <a href=\"#\" class=\"btn btn-primary\">Je m'inscrit</a>
+            <button type=\"button\"  class=\"btn btn-outline-primary inscrit".$identifiant."\" id=\"inscrit".$identifiant."-".$nom."\">".$message."</button>
             </div>
             <div class=\"card-footer text-muted\">".$interval->format('Dans %a jours')."
             </div>
