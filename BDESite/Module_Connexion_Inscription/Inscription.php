7551a974d12c3fb8633f2b6cb15271c667964aee
Akorus1 <Phiuris1@gmail.com>
2019-11-15 14:24:43 +0100

    Commit by Kevin

--- BDESite/Module_Connexion_Inscription/Inscription.php
+++ BDESite/Module_Connexion_Inscription/Inscription.php
@@ -9,66 +9,80 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>      
        
-        
+        <style type="text/css">
+        body {
+        background: url("https://media.discordapp.net/attachments/491542158164099072/644147980848463912/fond_cesi.png?width=948&height=586");
+        background-repeat: no-repeat;
+        background-size: cover;
+        }
+        #PremForm {
+        padding: 0px 100px 0px 100px; 
+        margin:130px 0px 0px 250px;
+        }
+        #ImageCesiDeux {
+        margin:-1028px 0px 0px 0px;     
+        }
+        </style>
+
         <title>Inscription</title>
 
-
-       
     </head>
     <body>
-    <div class="container">
- 
- <form class="form-signin" method="post" action="ScriptInscription.php">
+     <div class="container" id="PremForm">
+      <form class="form-signin" method="post" action="ScriptInscription.php">
  
    <h2 class="text-center">Inscription</h2>
    <label for="inputLastName" class="sr-only">Nom</label>
    <input type="text" id="inputNom" name="nom" class="form-control" placeholder="Votre Nom" required autofocus>
-
+   </br>
    <label for="inputFirstName" class="sr-only">Prénom</label>
    <input type="text" id="inputPrenom" name="prenom" class="form-control" placeholder="Votre Prenom" required>
-
-   <label for="inputCampus" class="sr-only">Campus</label>
-   <select name="campus" required>
-    <option value="Paris Nanterre">Campus de Paris Nanterre</option>
-    <option value="Reims">Campus de Reims</option>
-    <option value="Arras">Campus d'Arras</option>
-    <option value="Lille">Campus de Lille</option>
-    <option value="Nancy">Campus de Nancy</option>
-    <option value="Strasbourg">Campus de Strasbourg</option>
-    <option value="Dijon">Campus de Dijon</option>
-    <option value="Lyon">Campus de Lyon</option>
-    <option value="Nice">Campus de Nice</option>
-    <option value="Grenoble">Campus de Grenoble</option>
-    <option value="Aix-en-Provence">Campus d'Aix-en-Provence</option>
-    <option value="Toulouse">Campus de Toulouse</option>
-    <option value="Pau">Campus de Pau</option>
-    <option value="Bordeaux">Campus de Bordeaux</option>
-    <option value="Angoulême">Campus d'Angoulême</option>
-    <option value="La Rpchelle">Campus de La Rochelle</option>
-    <option value="Châteauroux">Campus de Châteauroux</option>
-    <option value="Le Mans">Campus du Mans</option>
-    <option value="Nantes">Campus de Nantes</option>
-    <option value="Saint-Nazaire">Campus de Saint-Nazaire</option>
-    <option value="Brest">Campus de Brest</option>
-    <option value="Saint-Nazaire">Campus de Saint-Nazaire</option>
-    <option value="Rouen">Campus de Rouen</option>
-    <option value="Caen">Campus de Caen</option>
-  </select> 
-
+   </br>
+   <div class="form-group">
+        <label for="exampleFormControlSelect1" class="sr-only">Campus </label>
+        <select class="form-control" name="campus">
+        <option value="Paris Nanterre">Campus de Paris Nanterre</option>
+        <option value="Reims">Campus de Reims</option>
+        <option value="Arras">Campus d'Arras</option>
+        <option value="Lille">Campus de Lille</option>
+        <option value="Nancy">Campus de Nancy</option>
+        <option value="Strasbourg">Campus de Strasbourg</option>
+        <option value="Dijon">Campus de Dijon</option>
+        <option value="Lyon">Campus de Lyon</option>
+        <option value="Nice">Campus de Nice</option>
+        <option value="Grenoble">Campus de Grenoble</option>
+        <option value="Aix-en-Provence">Campus d'Aix-en-Provence</option>
+        <option value="Toulouse">Campus de Toulouse</option>
+        <option value="Pau">Campus de Pau</option>
+        <option value="Bordeaux">Campus de Bordeaux</option>
+        <option value="Angoulême">Campus d'Angoulême</option>
+        <option value="La Rpchelle">Campus de La Rochelle</option>
+        <option value="Châteauroux">Campus de Châteauroux</option>
+        <option value="Le Mans">Campus du Mans</option>
+        <option value="Nantes">Campus de Nantes</option>
+        <option value="Saint-Nazaire">Campus de Saint-Nazaire</option>
+        <option value="Brest">Campus de Brest</option>
+        <option value="Saint-Nazaire">Campus de Saint-Nazaire</option>
+        <option value="Rouen">Campus de Rouen</option>
+        <option value="Caen">Campus de Caen</option>
+        </select>
+    </div>    
    <label for="inputEmail" class="sr-only">Adresse mail</label>
    <input type="text" id="inputEmail" name="email" class="form-control" placeholder="Adresse mail" required>
-   
-   <label for="inputPassword" class="sr-only">Mot de Passe</label>
-   <input type="password" id="inputPassword" name="password" class="form-control"  pattern=".{6,}" placeholder="Mot de passe" required>
-
-   <label for="inputStatus" class="sr-only">Status</label>
-   <select name="status" required>
+   </br>
+   <div class="form-group">
+   <label for="exampleFormControlSelect1" class="sr-only">Status </label>
+    <select class="form-control" name="status">
     <option value="1">Elève</option>
     <option value="2">Membre du BDE</option>
     <option value="3">Intervenant</option>
-   </select>
-
-    <button  class="btn btn-success btn-block" type="button" data-toggle="modal" data-target="#exampleModal">Inscription</button>
+    </select>
+   </div>
+   <label for="inputPassword" class="sr-only">Mot de Passe</label>
+   <input type="password" id="inputPassword" name="password" class="form-control"  pattern=".{6,}" placeholder="Mot de passe" required>    
+   
+   </br>        
+    <button class="btn btn-primary btn-block" type="button" data-toggle="modal" data-target="#exampleModal">Inscription</button>
     <a href="Connexion.php" target="blank">Déjà enregistré ? </a>
 
     <div class="modal bd-example-modal-lg" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="false">
@@ -125,5 +139,6 @@
 </div>
 </form>
 </div>
+<img  src="https://image.noelshack.com/fichiers/2019/46/5/1573816941-logo-cesi.png" width="400px" height="90px" id="ImageCesiDeux">
 </body>
 </html>
